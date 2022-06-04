47c7cac30fc8aa69a050f042a065f349be2804a4
Otávio <sedanootavio@gmail.com>
2022-06-04 19:14:51 -0300

    feat: add update

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -2,13 +2,10 @@ import User from "../models/user"
 
 class UserController {
   async store(req, res) {
-    const emailExist = await User.findOne({
-      where: {email: req.body.email}
-    })
     const idExist = await User.findOne({
       where: {id: req.body.id}
     })
-    if (emailExist || idExist) {
+    if (idExist) {
       return res.status(401).json({
         error: "Usuário já cadastrado"
       })
@@ -20,7 +17,32 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({msg: true})
+    const { email, oldPassword } = req.body
+    const user = await User.findByPk(req.userId)
+
+    if (email && email !== user.email) {
+      const emailExist = await User.findOne({
+        where: {email}
+      })
+
+      if ((await emailExist)) {
+        res.status(401).json({
+          error: "Usuário já cadastrado"
+        })
+      } 
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({
+        error: "Senha Inválida"
+      })
+    }
+
+    const { id, name, provider } = await user.update(req.body)
+
+    return res.status(200).json({
+      id, name, email, provider
+    })
   }
 }
 
