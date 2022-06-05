b63e745acf083fdc8b53bf57fb7a0c629016387a
Otávio <sedanootavio@gmail.com>
2022-06-04 23:20:08 -0300

    feat: confirmPassword

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -42,6 +42,9 @@ class UserController {
       password: Yup.string().min(6).when(
         'oldPassword', (oldPassword, field) => 
           oldPassword ? field.required() : field
+      ),
+      confirmPassword: Yup.string().when('password', (password, field) => 
+        password ? field.required().oneOf([Yup.ref('password')]) : field
       )
     })
 
