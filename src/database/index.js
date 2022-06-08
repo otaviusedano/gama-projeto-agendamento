fa07b878f8a07184c5e39ead853baa876422b26c
Otávio <sedanootavio@gmail.com>
2022-06-08 13:50:24 -0300

    feat: add mongoose

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from "sequelize"
+import mongoose from "mongoose"
 import User from '../app/models/user'
 import File from "../app/models/file" 
 import Appointment from "../app/models/appointment"
@@ -9,6 +10,7 @@ const models = [User, File, Appointment]
 class Database {
   constructor() {
     this.init()
+    this.mongo()
   }
   init() {
     this.connection = new Sequelize(databaseConfig)
@@ -16,6 +18,15 @@ class Database {
       .map(model => model.init(this.connection))
     User.associate(this.connection.models)
     Appointment.associate(this.connection.models)
+  }
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb+srv://agendamentos:agendamentos@cluster0.2g19ap7.mongodb.net/?retryWrites=true&w=majority', 
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      },
+    )
   }
 }
 
